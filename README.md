939c385474e01fce5ad66d5bc98e564f011ba6f5
Tilman M. Jaeschke <asciibeats@users.noreply.github.com>
2019-01-24 11:11:15 +0100

    Added a readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# textpoll
+A simple php framework with integrated template engine
+
+## Usage
+```
+cd <webserver_root_folder>
+git clone https://github.com/asciibeats/textpoll.git
+```
+and open `http://<webserver_domain_name>/textpoll/index.php?page=test` in your favorite browser.
--- modules/system/TextPoll.php
+++ modules/system/TextPoll.php
@@ -7,7 +7,7 @@ class TextPoll
   {
 		spl_autoload_register('self::autoload');
 		$controller = new Response();
-		$controller->addFilter(new filter\AccessFilter());
+		//$controller->addFilter(new filter\AccessFilter());
 		$controller->addFilter(new filter\TemplateFilter());
 		$controller->flush();
   }
